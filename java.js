0619978ed23468f152a5c8c2071657116fcb910b
mcleecueva <cuevamclee@gmail.com>
2025-05-15 18:33:07 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+
+  #scrollToTopBtn {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    background-color: #8575F0;
+    color: white;
+    border: none;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    font-size: 18px;
+    cursor: pointer;
+    display: none;
+    justify-content: center;
+    align-items: center;
+  }
+  #scrollToTopBtn:hover {
+    background-color: #6a5acd;
+  }
+
